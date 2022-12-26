84a70d7531f455b75ce8c70d9b7ed804575f3acc
Guillermo Berbesi <dev.guillermoberbesi@gmail.com>
2022-12-26 12:41:34 -0400

    plantilla de detalle

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //  $this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,22 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $products = Product::all();
+
+        return view('welcome', ['products' => $products]);
+    }
+
+    public function home()
+    {
+        $this->middleware('auth');
+
         return view('home');
     }
+
+    public function detail()
+    {
+        return view('detail');
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'home'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('welcome');
+Route::get('/detail', [App\Http\Controllers\HomeController::class, 'detail'])->name('detail');
 
